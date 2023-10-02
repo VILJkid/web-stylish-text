3b699e110aa4b78d3d2d5cf96839ad710842f510
VILJkid <sidvjmostwanted@gmail.com>
2023-10-03 00:11:07 +0530

    (fix) : organize js code

--- js/script.js
+++ js/script.js
@@ -1,30 +1,30 @@
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const span = document.querySelector(".overlay");
+
+function animateText() {
+    const targetText = span.textContent;
+    let iterations = 0;
+
+    const interval = setInterval(() => {
+        const newText = targetText
+            .split("")
+            .map((_, index) =>
+                index < iterations
+                    ? targetText[index]
+                    : letters[Math.floor(Math.random() * 26)]
+            )
+            .join("");
+
+        span.textContent = newText;
+
+        if (iterations >= targetText.length) {
+            clearInterval(interval);
+        }
+
+        iterations += 1 / 3;
+    }, 50);
+}
+
 window.onload = () => {
-    const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    const span = document.querySelector(".overlay");
-
-    function animateText() {
-        const targetText = span.textContent;
-        let iterations = 0;
-
-        const interval = setInterval(() => {
-            const newText = targetText
-                .split("")
-                .map((_, index) =>
-                    index < iterations
-                        ? targetText[index]
-                        : letters[Math.floor(Math.random() * 26)]
-                )
-                .join("");
-
-            span.textContent = newText;
-
-            if (iterations >= targetText.length) {
-                clearInterval(interval);
-            }
-
-            iterations += 1 / 3;
-        }, 50);
-    }
-
     animateText(); // Start the animation on page load
 };
