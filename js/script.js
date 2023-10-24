62afc5014107489a26cb5405f53f41888fb20280
VILJkid <sidvjmostwanted@gmail.com>
2023-10-24 19:39:25 +0530

    (fix) : follow proper convention

--- js/script.js
+++ js/script.js
@@ -1,21 +1,24 @@
 const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const text = document.querySelector("h1");
 
-function animateText() {
+function randomText(targetText, iterations) {
+    const newText = targetText
+        .split("")
+        .map((_, index) =>
+            index < iterations
+                ? targetText[index]
+                : letters[Math.floor(Math.random() * 26)]
+        )
+        .join("");
+    return newText
+}
+
+function animateText(text) {
     const targetText = text.textContent;
     let iterations = 0;
 
     const interval = setInterval(() => {
-        const newText = targetText
-            .split("")
-            .map((_, index) =>
-                index < iterations
-                    ? targetText[index]
-                    : letters[Math.floor(Math.random() * 26)]
-            )
-            .join("");
-
-        text.textContent = newText;
+        const newText = randomText(targetText, iterations);
+        text.textContent = newText
 
         if (iterations >= targetText.length) {
             clearInterval(interval);
@@ -25,11 +28,20 @@ function animateText() {
     }, 50);
 }
 
-window.onload = () => {
-    setTimeout(() => {
-        text.style.opacity = 1;
-    }, 700);
-    setTimeout(() => {
-        animateText();
-    }, 750);
-};
+function onWindowLoad(text) {
+    window.onload = () => {
+        setTimeout(() => {
+            text.style.opacity = 1;
+        }, 700);
+        setTimeout(() => {
+            animateText(text);
+        }, 750);
+    };
+}
+
+function animateRandomText(selector) {
+    const text = document.querySelector(selector);
+    onWindowLoad(text)
+}
+
+animateRandomText(".random-text")
